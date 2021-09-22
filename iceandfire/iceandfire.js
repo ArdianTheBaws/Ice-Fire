8c93ad88a517b910f6005be21b05e9d795a4fccb
ArdianTheBaws <54141216+ArdianTheBaws@users.noreply.github.com>
2021-09-23 00:19:19 +0200

    Add files via upload

---
+++ iceandfire/iceandfire.js
@@ -0,0 +1,20 @@
+ $(document).ready(function() {
+
+      $(document).on("click", "p", function(){
+          var id = this.id;
+          $.get(
+            `https://anapioficeandfire.com/api/houses/${id}`,
+            function(res) {
+              console.log(res);
+              var html_str = "";
+              html_str += "<h4>Name: " + res['name'] + "</h4>";
+              html_str += "<h4>Region:</h4>";
+              html_str += res['region'];
+              html_str += "<h4>Words:</h4>";
+              html_str += res['words'];
+              html_str += "<h4>Coat of Arms</h4>";
+              html_str += res['coatOfArms'];
+              $('#profile').html(html_str);
+          }, "json");
+          });
+        });
